042798f2ecf0abc5870834ccabc8b4e1614d8904
haidao9610 <324009610@qq.com>
2026-02-06 18:26:23 +0800

    update

--- src/common.rs
+++ src/common.rs
@@ -940,9 +940,10 @@ pub fn is_modifier(evt: &KeyEvent) -> bool {
 }
 
 pub fn check_software_update() {
-    if is_custom_client() {
-        return;
-    }
+//关闭检测自定义客户端不能升级检测
+//    if is_custom_client() {
+//        return;
+//    }
     let opt = LocalConfig::get_option(keys::OPTION_ENABLE_CHECK_UPDATE);
     if config::option2bool(keys::OPTION_ENABLE_CHECK_UPDATE, &opt) {
         std::thread::spawn(move || allow_err!(do_check_software_update()));
